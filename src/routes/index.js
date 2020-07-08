ffc74ca31905dff60a734265d6e3e45f43f922bf
dmchiodini <g25_diego@hotmail.com>
2020-07-08 19:15:49 -0300

    tratamento de erro para rotas nao encontradas

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,4 +7,12 @@ const router = new Router();
 router.use(orders);
 router.use(products);
 
+//Quando não encontrar rota, entra aqui:
+router.use((req, res, next) => {
+    const err = new Error('Não encontrado');
+    err.statu = 404;
+    next(err);
+});
+
+
 module.exports = router;
