f9737295c9fd63d5d9856b2679f82bffd23b0bda
dmchiodini <g25_diego@hotmail.com>
2020-07-09 19:56:50 -0300

    Adicionado migrations, knex e sqlite3

--- server.js
+++ server.js
@@ -1,31 +1,18 @@
 const express = require('express');
 const routes = require('./src/routes')
-const app = express();
 const morgan= require('morgan');
 const bodyParser = require('body-parser');
+const cors = require("cors");
+
+const app = express();
 const port = process.env.PORT || 5555;
 
-app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false })); // apenas dados simples
 app.use(bodyParser.json()); //json de entrada no body
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+app.use(morgan('dev'));
+app.use(cors());
 
-    if(req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).send({});
-    }
-    next();
-})
 app.use(routes);
-
-//Quando não encontrar rota, entra aqui:
-app.use((req, res, next) => {
-    const err = new Error('Não encontrado');
-    err.statu = 404;
-    next(err);
-});
 
 app.use((error, req, res, next)=>{
     res.status(error.status || 500)
